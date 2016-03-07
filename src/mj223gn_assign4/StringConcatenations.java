9a76b264cccc36a372ffa30dbbc989f545a225c4
xoften <mj223gn@student.lnu.se>
2016-03-07 21:38:39 +0100

    sda

--- src/mj223gn_assign4/StringConcatenations.java
+++ src/mj223gn_assign4/StringConcatenations.java
@@ -6,57 +6,61 @@ package mj223gn_assign4;
 public class StringConcatenations {
 
     public static void main(String[] args) {
-        //shortConcatenation();
+        shortConcatenation();
         longConcatenation();
-        //shortStringBuilder(10);
-        //longStringBuilder(10);
+        shortStringBuilder();
+        longStringBuilder();
 
     }
 
     public static void shortConcatenation() {
         String out = "";
         final long start = System.nanoTime();
-        for (int j = 0; j < 3000; j++) {
+        for (int j = 0; j < 35000; j++) {
             out += "y";
         }
         final long end = System.nanoTime();
-        System.out.println((end - start) / 1000000);
-        System.out.println(out.length());
+        System.out.println("Time is: " + (end - start) / 1000000 + " ms");
+        System.out.println("Number of chars in string: " + out.length());
+        System.out.println("Number of iterations: "+ out.length());
     }
 
     public static void longConcatenation() {
         String out = "";
         final long start = System.nanoTime();
-        for (int j = 0; j < 3150; j++) {
+        for (int j = 0; j < 4500; j++) {
             out += "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
         }
         final long end = System.nanoTime();
-        System.out.println((end - start) / 1000000);
-        System.out.println((out.length()/80));
+        System.out.println("Time is: " + (end - start) / 1000000 + " ms");
+        System.out.println("Number of chars in string: " + out.length());
+        System.out.println("Number of iterations: "+(out.length()/80));
     }
 
-    public static void shortStringBuilder(int n) {
+    public static void shortStringBuilder() {
         StringBuilder out = new StringBuilder();
         final long start = System.nanoTime();
-        for (int j = 0; j < 70000000; j++) {
+        for (int j = 0; j < 130000000; j++) {
             out.append("x");
         }
         out.toString();
         final long end = System.nanoTime();
-        System.out.println((end - start) / 1000000);
-        System.out.println(out.toString().length());
+        System.out.println("Time is: " + (end - start) / 1000000 + " ms");
+        System.out.println("Number of chars in string: " + out.toString().length());
+        System.out.println("Number of iterations: "+ out.length());
     }
 
-    public static void longStringBuilder(int n) {
+    public static void longStringBuilder() {
         StringBuilder out = new StringBuilder();
         final long start = System.nanoTime();
-        for (int j = 0; j < 3300000; j++) {
-            out.append("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        for (int j = 0; j < 4299999; j++) {
+            out.append("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
         }
         out.toString();
         final long end = System.nanoTime();
-        System.out.println((end - start) / 100000000);
-        System.out.println(out.toString().length());
+        System.out.println("Time is: " + (end - start) / 1000000 + " ms");
+        System.out.println("Number of chars in string: " + out.toString().length());
+        System.out.println("Number of iterations: "+(out.length()/80));
     }
 
 
---
+++ src/mj223gn_assign4/sortTiming.java
@@ -0,0 +1,90 @@
+package mj223gn_assign4;
+
+
+import mj223gn_assign3.Algorithms.SortingAlgorithms;
+
+import java.util.Random;
+
+/**
+ * Created by Michaels on 2016-03-07.
+ */
+public class sortTiming {
+
+    public static void main(String[] args) {
+
+        insertionSortIntTest(10, createIntArray(111000));
+        mergeSortIntTest(10, createIntArray(7200000));
+        insertionSortStringTest(10, createStringArray(11000));
+        mergeSortStringTest(10, createStringArray(1000000));
+    }
+    public static int[] createIntArray(int n){
+        Random random = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++){
+            array[i] = random.nextInt(n*2);
+        }
+        return array;
+    }
+    public static String[] createStringArray(int n){
+        Random random = new Random();
+        String[] array = new String[n];
+        for (int i = 0; i < n; i++){
+            String out = "";
+            for (int j = 0; j < 10; j++){
+                int cha = random.nextInt(25)+97;
+                out += (char) cha;
+            }
+            array[i] = out;
+        }
+        return array;
+    }
+    public static void insertionSortIntTest(int numberOfRuns, int[] array) {
+        int time = 0;
+        for (int i = 0; i < numberOfRuns; i++) {
+            final long start = System.nanoTime();
+            SortingAlgorithms.insertionSort(array);
+            final long end = System.nanoTime();
+            time += ((end - start) / 1000000);
+
+        }
+        System.out.println(time/numberOfRuns);
+        System.out.println(array.length);
+    }
+    public static void mergeSortIntTest(int numberOfRuns, int[] array){
+        int time = 0;
+        for (int i = 0; i < numberOfRuns; i++) {
+            final long start = System.nanoTime();
+            SortingAlgorithms.mergeSort(array);
+            final long end = System.nanoTime();
+            time += ((end - start) / 1000000);
+
+        }
+        System.out.println(time/numberOfRuns);
+        System.out.println(array.length);
+    }
+    public static void insertionSortStringTest(int numberOfRuns, String[] array) {
+        int time = 0;
+        for (int i = 0; i < numberOfRuns; i++) {
+            final long start = System.nanoTime();
+            SortingAlgorithms.insertionSort(array);
+            final long end = System.nanoTime();
+            time += ((end - start) / 1000000);
+
+        }
+        System.out.println(time/numberOfRuns);
+        System.out.println(array.length);
+    }
+    public static void mergeSortStringTest(int numberOfRuns, String[] array){
+        int time = 0;
+        for (int i = 0; i < numberOfRuns; i++) {
+            final long start = System.nanoTime();
+            SortingAlgorithms.mergeSort(array);
+            final long end = System.nanoTime();
+            time += ((end - start) / 1000000);
+
+        }
+        System.out.println(time/numberOfRuns);
+        System.out.println(array.length);
+    }
+
+}
