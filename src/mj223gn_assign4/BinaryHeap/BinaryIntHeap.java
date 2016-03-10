688f78be028f0040efad18d57076ee42193027eb
xoften <mj223gn@student.lnu.se>
2016-03-10 16:08:07 +0100

    ijii

--- src/mj223gn_assign4/BinaryHeap/BinaryIntHeap.java
+++ src/mj223gn_assign4/BinaryHeap/BinaryIntHeap.java
@@ -1,6 +1,8 @@
 package mj223gn_assign4.BinaryHeap;
 
 
+import java.util.Arrays;
+
 /**
  * Created by Michael Johansson(mj223gn) on 2016-03-09.
  */
@@ -16,10 +18,23 @@ public class BinaryIntHeap {
 
     }
     public void insert(int n) {
+        if(isEmpty()){
+            binaryHeap[++size] = n;
+        }
+        else{
+            binaryHeap[++size] = n;
+            percolationUp();
+            if(size+1 == binaryHeap.length){
+                resize();
+            }
+        }
+
 
     }
     public int pullHighest(){
-        return 0;
+        int highest = binaryHeap[1];
+        percolationDown();
+        return highest;
     }
     public int size(){
         return size;
@@ -28,12 +43,59 @@ public class BinaryIntHeap {
         return size == 0;
     }
     public void resize(){
-
+        int[] temp = new int[binaryHeap.length*2];
+        for (int i = 0; i < binaryHeap.length; i++){
+            temp[i] = binaryHeap[i];
+        }
+        binaryHeap = temp;
+    }
+    public String toString(){
+        return Arrays.toString(this.binaryHeap);
     }
     private void percolationUp(){
-
+        int index = size;
+        while(binaryHeap[index] > binaryHeap[index/2] && index > 1){
+            int temp = binaryHeap[index];
+            binaryHeap[index] = binaryHeap[index/2];
+            binaryHeap[index/2] = temp;
+            index = index/2;
+        }
     }
-    private class Node {
-
+    private void percolationDown(){
+        int index = 1;
+        binaryHeap[index] = binaryHeap[size];
+        binaryHeap[size] = 0;
+        while (binaryHeap[2*index] > binaryHeap[2*index+1]){
+            if(binaryHeap[2*index] > binaryHeap[index]){
+                System.out.println("kalle");
+                int temp = binaryHeap[index];
+                binaryHeap[index] = binaryHeap[2*index];
+                binaryHeap[2*index] = temp;
+                index = 2*index;
+            }
+            else if (binaryHeap[2 * index + 1] > binaryHeap[index]) {
+                System.out.println("patrik");
+                int temp = binaryHeap[index];
+                binaryHeap[index] = binaryHeap[2 * index];
+                binaryHeap[2 * index] = temp;
+                index = 2*index+1;
+            }
+        }
+        while (binaryHeap[2*index] < binaryHeap[2*index+1]) {
+            if (binaryHeap[2 * index + 1] > binaryHeap[index]) {
+                System.out.println("Michael");
+                int temp = binaryHeap[index];
+                binaryHeap[index] = binaryHeap[2 * index];
+                binaryHeap[2 * index] = temp;
+                index = 2*index+1;
+            }
+            else if(binaryHeap[2*index] > binaryHeap[index]){
+                System.out.println("bajs");
+                int temp = binaryHeap[index];
+                binaryHeap[index] = binaryHeap[2*index];
+                binaryHeap[2*index] = temp;
+                index = 2*index;
+            }
+        }
     }
 }
--- src/mj223gn_assign4/BinaryHeap/test.java
+++ src/mj223gn_assign4/BinaryHeap/test.java
@@ -7,11 +7,21 @@ public class test {
 
     public static void main(String[] args) {
         BinaryIntHeap bih = new BinaryIntHeap();
+
         bih.insert(10);
         bih.insert(8);
         bih.insert(9);
         bih.insert(13);
+        bih.insert(98);
+        bih.insert(5);
+        bih.insert(76);
+        bih.insert(14);
+        bih.insert(1);
+        System.out.println(bih.size());
 
+        System.out.println(bih.pullHighest());
+
+        System.out.println(bih.toString());
 
     }
 }
